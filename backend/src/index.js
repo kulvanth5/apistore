5d076712a22ee559b1e986b85299a8ef4112450f
kulvanth5 <kulvanth5@gmail.com>
2022-03-05 11:49:08 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,7 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(express.json({ limit: "1mb" }));
+app.use(express.static("public"));
 
 const corsOrigin = "http://localhost:3000";
 app.use(
@@ -34,16 +35,10 @@ mongoose
     console.log("error occured" + err);
   });
 
-app.get("/", (req, res) => {
-  res.send(express.static("public"));
-});
-
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/image-upload", getImage);
 app.use("/api/products", apiRoutes);
-
-// app.use(express.static("public"));
 
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
