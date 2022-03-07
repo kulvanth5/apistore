51511dc82e3f7b7bafe7234525d9581acac2a25e
kulvanth5 <kulvanth5@gmail.com>
2022-03-07 09:16:46 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const path = require("path");
+// const path = require("path");
 dotenv.config({ path: __dirname + "/.env" });
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/auth");
@@ -13,7 +13,7 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(express.json({ limit: "1mb" }));
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+app.use(express.static("public"));
 
 const corsOrigin = "http://localhost:3000";
 app.use(
