2277adaa4392472634e71e0a68069ad9020afc3e
kulvanth5 <kulvanth5@gmail.com>
2022-03-11 17:03:08 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,8 @@
-/* eslint-disable no-global-assign */
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const path = require("path");
+// const path = require("path");
 dotenv.config({ path: __dirname + "/.env" });
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/auth");
@@ -14,8 +13,9 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(express.json({ limit: "1mb" }));
+app.use(express.static("build"));
 
-const corsOrigin = "http://localhost:3000";
+const corsOrigin = "https://store909.herokuapp.com/";
 app.use(
   cors({
     origin: [corsOrigin],
@@ -41,20 +41,6 @@ app.use("/image-upload", getImage);
 app.use("/api/products", apiRoutes);
 
 let port = process.env.PORT || 5000;
-
-__dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "src/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
 
 app.listen(port, () => {
   console.log("backend running at" + port);
