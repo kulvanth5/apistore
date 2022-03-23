890745da5f24b967583364de951fc473c440e1e2
kulvanth5 <kulvanth5@gmail.com>
2022-03-23 19:02:55 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const corsOrigin = "http://localhost:3000";
 app.use(express.json({ limit: "1mb" }));
-// app.use(express.static("public/build"));
 
 app.use(
   cors({
@@ -34,26 +33,19 @@ mongoose
     console.log("error occured" + err);
   });
 
-app.get("/main", (req, res) => {
-  res.send("test");
-});
-
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/image-upload", getImage);
 app.use("/api/products", apiRoutes);
 
-app.use(express.static(path.join(__dirname, "./src/build")));
-
-app.get("*", function (_, res) {
-  res.sendFile(path.join(__dirname, "./src/build/index.html"), function (err) {
-    if (err) {
-      res.status(500).send(err);
-    }
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
-});
+}
 
-let port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 app.listen(port, () => {
   console.log("backend running at" + port);
